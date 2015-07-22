295b22b31df77eb3e2915a69443ed2fd1895c1c4
Patrick Morris <prmwrites@gmail.com>
2015-07-22 18:25:15 -0400

    Finished Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Friend or Foe"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Friend or Foe"
   end
 
+  test "should get countries" do
+    get :countries
+    assert_response :success
+    assert_select "title", "Countries | Friend or Foe"
+  end
 end

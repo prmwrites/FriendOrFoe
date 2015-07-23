7a2b7cd153adb6d3a1176eeccb898d14d329074d
Patrick Morris <prmwrites@gmail.com>
2015-07-22 21:42:09 -0400

    Made a basic Country model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'countries/new'
+
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'countries'   => 'static_pages#countries'

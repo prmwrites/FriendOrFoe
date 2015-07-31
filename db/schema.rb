6a10d49626db5ffbf42a7fde58f76123dc877475
sproutsolutions <george@sproutsolutions.us>
2015-07-30 20:43:37 -0400

    Removed CSV and SQL files

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724002124) do
+ActiveRecord::Schema.define(version: 20150723004724) do
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.text     "opinion_of_usa"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

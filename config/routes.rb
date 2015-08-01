d81c05f16e2f2ee7f6272b3172f9908988aee53d
Patrick Morris <prmwrites@gmail.com>
2015-08-01 18:57:11 -0400

    Added basic nested routes (countries/id/relations/id)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
 
+  resources :relations
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'countries'   => 'static_pages#countries'
-  resources :countries
+  resources :countries do
+    resources :relations
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+    #Countries
 
     Canada = Country.create([{ name: 'Canada', opinion_of_usa: 'very friendly' }])
-
-    Mexico = Country.create([{ name: 'Mexico', opinion_of_usa: 'friendly' }])
 
     Iran = Country.create([{ name: 'Iran', opinion_of_usa: 'very unfriendly' }])
 
     Iraq = Country.create([{ name: 'Iraq', opinion_of_usa: 'unfriendly' }])
+
+    Mexico = Country.create([{ name: 'Mexico', opinion_of_usa: 'friendly' }])
+
+    # Relations
+
+    Canada2 = Relation.create([{ name: 'Canada', parent_id: 1 }])
+
+    Iran2 = Relation.create([{ name: 'Iran', parent_id: 2 }])
+
+    Iraq2 = Relation.create([{ name: 'Iraq', parent_id: 3 }])
+
+    Mexico2 = Relation.create([{ name: 'Mexico', parent_id: 4 }])
